cf5f007959fd67219ccf5ef41cbf071a147b8970
Hafiz2023 <adilamin374@gmail.com>
2023-08-23 12:05:17 +0500

    update

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -38,7 +38,7 @@ const HeaderMain = () => {
 
           <div className="relative">
             <HiOutlineShoppingBag />
-            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
+            <div className="bg-red-500 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">
               0
             </div>
           </div>
